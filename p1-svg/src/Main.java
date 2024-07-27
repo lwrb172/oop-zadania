f9841b45711b7f9364bee47ed89d4f9246d21648
lwrb172 <lwrbielak@gmail.com>
2024-07-27 03:32:16 +0200

    lab1z2

--- p1-svg/src/Main.java
+++ p1-svg/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         Point p2 = new Point (3, 4);
         Segment s1 = new Segment(p1, p2);
         System.out.println(s1.Length());
+        System.out.println(s1.toSvg());
     }
 }
--- p1-svg/src/Segment.java
+++ p1-svg/src/Segment.java
@@ -10,19 +10,18 @@ public class Segment {
         return p1;
     }
 
-//    public void setP1(Point p1) {
-//        this.p1 = p1;
-//    }
-
     public Point getP2() {
         return p2;
     }
 
-//    public void setP2(Point p2) {
-//        this.p2 = p2;
-//    }
-
     public double Length() {
         return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
+
+    public String toSvg() {
+        return "<svg height=\"200\" width=\"300\" xmlns=\"http://www.w3.org/2000/svg\"> " +
+                "<line x1=\"" + p1.x +"\" y1=\"" + p1.y + "\" x2=\"" + p2.x + "\" y2=\"" + p2.y +
+                "\" style=\"stroke:black;stroke-width:2\" /> " +
+                "</svg>";
     }
 }
