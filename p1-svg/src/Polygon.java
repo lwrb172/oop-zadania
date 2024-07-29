0412452fe68b3cd3f473b629d751cf6d2e563333
lwrb172 <lwrbielak@gmail.com>
2024-07-29 20:49:21 +0200

    lab2z1

--- p1-svg/src/Polygon.java
+++ p1-svg/src/Polygon.java
@@ -1,8 +1,15 @@
 public class Polygon {
     private Point[] points;
+    private Style style;
 
     public Polygon(Point[] points) {
         this.points = points;
+        this.style = new Style();
+    }
+
+    public Polygon(Point[] points, Style style) {
+        this.points = points;
+        this.style = style;
     }
 
     public Polygon(Polygon other) {
@@ -11,6 +18,7 @@ public class Polygon {
             pointsCopy[i] = new Point(other.points[i].getX(), other.points[i].getY());
         }
         this.points = pointsCopy;
+        this.style = new Style(other.style.getFillColor(), other.style.getStrokeColor(), other.style.getStrokeWidth());
     }
 
     public String toSvg() {
@@ -20,7 +28,7 @@ public class Polygon {
         for (Point point : points) {
             sb.append(point.x).append(",").append(point.y).append(" ");
         }
-        sb.append("\"").append(" /> ").append("</svg>");
+        sb.append("\" ").append(this.style.toSvg()).append(" /> ").append("</svg>");
         return sb.toString();
     }
 }
