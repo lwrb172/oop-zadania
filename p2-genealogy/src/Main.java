f675fd8fa3cc1c59e9a1a02b1f11f0e8520bb06c
lwrb172 <lwrbielak@gmail.com>
2024-08-21 02:12:05 +0200

    lab4z2

--- p2-genealogy/src/Main.java
+++ p2-genealogy/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(Person.fromCsvLine("Marek Kowalski,15.05.1899,25.06.1957,,"));
+
+        List<Person> people = Person.fromCsv("p2-genealogy/family.csv");
+        for (Person person : people) {
+            System.out.println(person);
+        }
     }
 }
--- p2-genealogy/src/Person.java
+++ p2-genealogy/src/Person.java
@@ -1,5 +1,8 @@
+import java.io.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Person {
     public String name;
@@ -18,6 +21,23 @@ public class Person {
         LocalDate birthDate = LocalDate.parse(buffer[1], formatter);
         LocalDate deathDate = buffer[2].isEmpty() ? null : LocalDate.parse(buffer[2], formatter);
         return new Person(buffer[0], birthDate, deathDate);
+    }
+
+    public static List<Person> fromCsv(String path) {
+        File file = new File(path);
+        List<Person> people = new ArrayList<>();
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                    people.add(Person.fromCsvLine(line));
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("Error reading CSV file" + e.getMessage());
+        }
+        return people;
     }
 
     @Override
