f88d856ac432263642bcc5d476e1fb344a3849d7
lwrb172 <lwrbielak@gmail.com>
2024-09-08 13:38:20 +0200

    lab7z3

--- p5-site/src/main/java/auth/Account.java
+++ p5-site/src/main/java/auth/Account.java
@@ -1,4 +1,11 @@
 package auth;
 
 public record Account(int id, String username) {
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
 }
