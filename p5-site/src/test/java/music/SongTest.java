d3e7a99fe458811cfffd6fae5520fa1cf090271e
lwrb172 <lwrbielak@gmail.com>
2024-09-08 18:28:21 +0200

    lab8z2d

--- p5-site/src/test/java/music/SongTest.java
+++ p5-site/src/test/java/music/SongTest.java
@@ -4,8 +4,12 @@ import database.DatabaseConnection;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,6 +33,24 @@ public class SongTest {
         Optional<Song> testSong = Song.Persistence.read(53);
         assertFalse(testSong.isPresent());
     }
+
+     static private Stream<Arguments> provideSongData() {
+        return Stream.of(
+                Arguments.of(5, "Queen", "Bohemian Rhapsody", 355),
+                Arguments.of(25, "John Lennon", "Imagine", 183),
+                Arguments.of(1, "The Beatles", "Hey Jude", 431)
+        );
+     }
+
+     @ParameterizedTest
+     @MethodSource("provideSongData")
+     void readSongs(int id, String artist, String title, int duration) {
+        Optional<Song> testSong = Song.Persistence.read(id);
+        Song expectedSong = new Song(artist, title, duration);
+        assertTrue(testSong.isPresent());
+        assertEquals(expectedSong, testSong.get());
+     }
+
 
     @AfterAll
     static void closeDatabase() {
