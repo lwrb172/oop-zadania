4e347c9b8ec75ae65d2d8441bff4cc9cb943fb43
lwrb172 <lwrbielak@gmail.com>
2024-09-08 14:12:13 +0200

    lab8z1c

--- p5-site/src/test/java/music/PlaylistTest.java
+++ p5-site/src/test/java/music/PlaylistTest.java
@@ -2,6 +2,7 @@ package music;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlaylistTest {
@@ -9,5 +10,12 @@ public class PlaylistTest {
     void testEmptyPlaylist() {
         Playlist playlist = new Playlist();
         assertTrue(playlist.isEmpty());
+    }
+
+    @Test
+    void testSingleElement() {
+        Playlist playlist = new Playlist();
+        playlist.add(new Song("Atr", "000", 180));
+        assertEquals(1, playlist.size());
     }
 }
