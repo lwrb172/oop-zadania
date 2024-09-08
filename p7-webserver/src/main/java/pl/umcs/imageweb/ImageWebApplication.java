dccbf7df644bddb84a3b2d8334afb0b5c7b0907e
lwrb172 <lwrbielak@gmail.com>
2024-09-08 21:40:01 +0200

    lab10z1

---
+++ p7-webserver/src/main/java/pl/umcs/imageweb/ImageWebApplication.java
@@ -0,0 +1,13 @@
+package pl.umcs.imageweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageWebApplication.class, args);
+	}
+
+}
---
+++ p7-webserver/src/main/java/pl/umcs/imageweb/RectangleController.java
@@ -0,0 +1,12 @@
+package pl.umcs.imageweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RectangleController {
+    @GetMapping
+    public String hello() {
+        return "Hello world";
+    }
+}
