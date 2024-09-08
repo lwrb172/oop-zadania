4b11146f7ef5eb6f3139d84b3b819eb3f102b360
lwrb172 <lwrbielak@gmail.com>
2024-09-08 23:17:48 +0200

    lab11z1

---
+++ p8-chat_server/src/main/java/pl/umcs/Client.java
@@ -0,0 +1,31 @@
+package pl.umcs;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client implements Runnable {
+    private Server server;
+    private BufferedReader reader;
+    private PrintWriter writer;
+
+    public Client(Server server, Socket socket) throws IOException {
+        this.server = server;
+        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+    }
+
+    @Override
+    public void run() {
+        String message;
+        try {
+            while ((message = reader.readLine()) != null)
+                server.broadcast(message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void send(String message) {
+        this.writer.println(message);
+    }
+}
