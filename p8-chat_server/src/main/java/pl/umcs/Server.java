c865eaf22ba9774f7f9b9da56a37304261f452c8
lwrb172 <lwrbielak@gmail.com>
2024-09-10 16:36:20 +0200

    lab11z3a

--- p8-chat_server/src/main/java/pl/umcs/Server.java
+++ p8-chat_server/src/main/java/pl/umcs/Server.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Server {
     private ServerSocket ss;
     private List<Client> clients = new ArrayList<>();
+    private Map<String, Client> clientMap = new HashMap<>();
 
 
     public Server() throws IOException {
@@ -27,5 +30,21 @@ public class Server {
     public void broadcast(String message) {
         for (Client client : clients)
             client.send(message);
+    }
+
+    public void clientLogged(Client loggedClient) {
+        clientMap.put(loggedClient.getLogin(), loggedClient);
+        for (Client currenClient : clients)
+            if (currenClient != loggedClient)
+                currenClient.send(String.format("%s logged in", loggedClient.getLogin()));
+    }
+
+    public void clientLeft(Client leavingClient) {
+        clients.remove(leavingClient);
+        try {
+            clientMap.remove(leavingClient.getLogin());
+        } catch (Exception _) {}
+        for (Client currentClient : clients)
+            currentClient.send(String.format("%s logged off", leavingClient.getLogin()));
     }
 }
